9ae14dc11486f49a205e05db7e807f7346ddb517
LuucasFeelix <77838383+LuucasFeelix@users.noreply.github.com>
2024-06-16 03:04:13 -0300

    Quase pronto tudo o que tinha usando guna

--- ListAnime/Form1.Designer.cs
+++ ListAnime/Form1.Designer.cs
@@ -28,35 +28,100 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.painelLateral = new System.Windows.Forms.Panel();
+            this.imgListAnime = new System.Windows.Forms.PictureBox();
+            this.btnAssistido = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.painelLateral.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imgListAnime)).BeginInit();
             this.SuspendLayout();
             // 
-            // checkBox1
+            // painelLateral
             // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(406, 78);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(80, 17);
-            this.checkBox1.TabIndex = 0;
-            this.checkBox1.Text = "checkBox1";
-            this.checkBox1.UseVisualStyleBackColor = true;
+            this.painelLateral.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(106)))), ((int)(((byte)(128)))), ((int)(((byte)(144)))));
+            this.painelLateral.Controls.Add(this.button2);
+            this.painelLateral.Controls.Add(this.button1);
+            this.painelLateral.Controls.Add(this.btnAssistido);
+            this.painelLateral.Controls.Add(this.imgListAnime);
+            this.painelLateral.Location = new System.Drawing.Point(0, 0);
+            this.painelLateral.Name = "painelLateral";
+            this.painelLateral.Size = new System.Drawing.Size(215, 604);
+            this.painelLateral.TabIndex = 0;
+            // 
+            // imgListAnime
+            // 
+            this.imgListAnime.Image = global::ListAnime.Properties.Resources.Logo1;
+            this.imgListAnime.Location = new System.Drawing.Point(12, 22);
+            this.imgListAnime.Name = "imgListAnime";
+            this.imgListAnime.Size = new System.Drawing.Size(175, 47);
+            this.imgListAnime.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.imgListAnime.TabIndex = 0;
+            this.imgListAnime.TabStop = false;
+            // 
+            // btnAssistido
+            // 
+            this.btnAssistido.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(122)))), ((int)(((byte)(145)))), ((int)(((byte)(161)))));
+            this.btnAssistido.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnAssistido.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.btnAssistido.ForeColor = System.Drawing.Color.White;
+            this.btnAssistido.Location = new System.Drawing.Point(0, 152);
+            this.btnAssistido.Name = "btnAssistido";
+            this.btnAssistido.Size = new System.Drawing.Size(187, 47);
+            this.btnAssistido.TabIndex = 1;
+            this.btnAssistido.Text = "Animes Assistidos";
+            this.btnAssistido.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(122)))), ((int)(((byte)(145)))), ((int)(((byte)(161)))));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(0, 271);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(187, 47);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Animes Para Assistir";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(122)))), ((int)(((byte)(145)))), ((int)(((byte)(161)))));
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(0, 393);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(187, 47);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Adicionar Anime";
+            this.button2.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.checkBox1);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(122)))), ((int)(((byte)(145)))), ((int)(((byte)(161)))));
+            this.ClientSize = new System.Drawing.Size(553, 604);
+            this.Controls.Add(this.painelLateral);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.painelLateral.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.imgListAnime)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Panel painelLateral;
+        private System.Windows.Forms.PictureBox imgListAnime;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnAssistido;
     }
 }
 
